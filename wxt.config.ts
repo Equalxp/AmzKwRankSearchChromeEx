b95c61a372504fce606eaac1cf962d1899ae02b9
Equalxp <2106705593@qq.com>
2025-07-30 14:13:54 +0800

    style: "format code"

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-vue'],
+    modules: ['@wxt-dev/module-vue'],
+    manifest: {
+        name: '亚马逊关键词排名查询',
+        description: '在亚马逊搜索结果页上定位ASIN，获取排名，支持批量导入Excel关键词表',
+        version: '1.0.0',
+        permissions: [
+            'storage',
+            'activeTab'
+        ],
+    }
 });
